6d67d00189bf07e7b68d5fe89d8e5377d65a00c2
Yagiz ALP <yagizwasmeow@gmail.com>
2020-06-18 20:32:10 +0300

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,11 @@
-client_script('client.lua')
+client_script 'client.lua'
 
 server_scripts {
     '@mysql-async/lib/MySQL.lua',
     'server.lua'
 }
 
-ui_page('html/index.html')
+ui_page 'index.html'
 
 files {
     'html/index.html',
